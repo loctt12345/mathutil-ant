67abd923f5fa91e126f08163015f0a55c2c0ef39
loctt12345 <trnthinlc@gmail.com>
2023-02-20 14:27:16 +0700

    add one more testcase

--- test/com/minhncn/mathutil/core/test/MathUtilityAdvancedTest.java
+++ test/com/minhncn/mathutil/core/test/MathUtilityAdvancedTest.java
@@ -28,7 +28,8 @@ public class MathUtilityAdvancedTest {
             {2, 2},
             {3, 6},
             {4, 24},
-            {5, 120}
+            {5, 120},
+            {6, 720}
         };
     }
     
